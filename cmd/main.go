4d227f3f28c7df4d4b565f35c99df0dac718c9f5
Shinya Ishikawa <s.ishikawa.gojo@gmail.com>
2019-01-24 22:46:05 +0900

    main.goのbridgeパターンの処理を修正、各パターンの前後に開始処理と終了処理を追加

--- cmd/main.go
+++ cmd/main.go
@@ -24,45 +24,54 @@ func main() {
 }
 
 func executeIterator() {
+	start("Iteraotr")
 	bookShelf := itr.BookShelf{Last: 0}
 	bookShelf.AppendBook(itr.Book{Name: "Around the World in 80 days"})
 	bookShelf.AppendBook(itr.Book{Name: "Bidle"})
 	bookShelf.AppendBook(itr.Book{Name: "Cinderella"})
 	it := bookShelf.Iterator()
 	for it.HasNext() {
+		// print book name
 		fmt.Println(it.Next())
 	}
+	end("Iteraotr")
 }
 
 func executeAdapter() {
+	start("Adapter")
 	pb := adp.NewPrintBanner("Black Friday SALE!")
 	pb.PrintWeak()
 	pb.PrintStrong()
+	end("Adapter")
 }
 
 func executeTemplateMethod() {
+	start("Template method")
 	cd := tm.NewCharDisplay("go")
 	cdc := tm.NewDisplay(cd)
 	cdc.Display()
 	sd := tm.NewStringDisplay("gopher is nice.", 10)
 	sdc := tm.NewDisplay(sd)
 	sdc.Display()
+	end("Template method")
 }
 
 func executeFactoryMethod() {
-	//create IDCardFactory instance.
+	start("Factory method")
 	cf := fm.NewIDCardFactory()
-	//create IDCard instance.
 	idc := fm.CreateInstance("ゆうきひろし", cf)
-	//IDcard use.
 	idc.Use()
+	end("Factory method")
 }
 
 func executeSingleton() {
+	start("Singleton")
 	sin.GetInstance()
+	end("Singleton")
 }
 
 func executePrototype() {
+	start("Prototype")
 	m := pr.NewManager()
 	// create instance
 	upen := pr.NewUnderlinePen("~")
@@ -81,11 +90,18 @@ func executePrototype() {
 	p2.Use("Hello, world")
 	p3 := m.Create("slash box")
 	p3.Use("Hello, world")
+	end("Prototype")
 }
 
 func executeBuilder() {
+	start("Builder")
 	flag.Parse()
 	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Builder is required parameter.")
+		fmt.Println("Use plain or html.")
+		return
+	}
 	if args[0] == "plain" {
 		b := bld.NewTextBuilder()
 		d := bld.NewDirector(b)
@@ -99,4 +115,13 @@ func executeBuilder() {
 		fileNm := b.GetResult()
 		fmt.Println(fileNm + " が作成されました")
 	}
+	end("Builder")
+}
+
+func start(name string) {
+	fmt.Println("Start " + name + " pattern. ")
+}
+
+func end(name string) {
+	fmt.Println("End " + name + " pattern. \n")
 }
